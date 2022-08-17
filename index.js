d5ab459b9bdea338e63e129a24659844c0886582
Jiroge <s.jiroge@gmail.com>
2022-08-17 14:41:03 +0700

    first

--- index.js
+++ index.js
@@ -3,22 +3,29 @@ const app = express ();
 const userRouter = require ("./routes/userRoute");
 const { default: mongoose } = require('mongoose');
 const { json } = require("express");
-
+const config = require ("./config") 
+// const cors = require ("cors");
 const PORT = 8080;
 
-app.get ('/', (req, res) => {
-    res.send ("<h1>Hello Express</h1>");
-});
+console.log (config)
 
+// app.use(cors());
 app.use (express.json ());
 app.use (express.urlencoded ({extended: false}));
 
 app.use ('/users', userRouter);
 
+app.get ('/', (req, res) => {
+  res.send ("<h1>Hello Express</h1>");
+});
+
 const start = async () => {
     // DO NOT COMMIT/PUSH USERNAME AND PASSWORD TO Github
-    await mongoose.connect(
-      'mongodb+srv://final101:final101@cluster001.zibw8bw.mongodb.net/?retryWrites=true&w=majority'
+    await mongoose.connect ( config.mongodb.uri, {
+      user: config.mongodb.username,
+      pass: config.mongodb.password,
+      retryWrites: true,
+    }
       , { dbName: "test" }
     );
     app.listen(PORT, () => {
@@ -27,3 +34,5 @@ const start = async () => {
   };
   
 start();
+
+const cors = require('cors');
